a565013c45bfdc1f616d68460ba534a1d5353235
Christian Bulitta <github@bulitta.com>
2014-06-23 18:30:40 +0100

    vagrant: create hadoop cluster (5 machines)

---
+++ 04_implementation/provisioning/ambari-provisioning/modules/etchosts/manifests/init.pp
@@ -0,0 +1,48 @@
+# Ensure that the machines in the cluster can find each other without DNS
+class etchosts ($ownhostname) {
+  host { 'host_one':
+    name  => 'one.cluster',
+    alias => ['one', 'one.cluster'],
+    ip    => '192.168.66.101'
+  }
+
+  host { 'host_two':
+    name  => 'two.cluster',
+    alias => ['two', 'two.cluster'],
+    ip    => '192.168.66.102'
+  }
+
+  host { 'host_three':
+    name  => 'three.cluster',
+    alias => ['three', 'three.cluster'],
+    ip    => '192.168.66.103'
+  }
+
+  host { 'host_four':
+    name  => 'four.cluster',
+    alias => ['four', 'four.cluster'],
+    ip    => '192.168.66.104'
+  }
+
+  host { 'host_five':
+    name  => 'five.cluster',
+    alias => ['five', 'five.cluster'],
+    ip    => '192.168.66.103'
+  }
+
+  file { 'agent_hostname':
+    path    => "/etc/hostname",
+    ensure  => present,
+    replace => true,
+    content => "${ownhostname}", # own hostname
+    owner   => 1546
+  }
+
+  file { 'agent_sysconfig':
+    path    => "/etc/sysconfig/network",
+    ensure  => present,
+    replace => true,
+    content => "NETWORKING=yes \nHOSTNAME=${ownhostname}" # own hostname
+  }
+
+}
