d67b4cf1e2e0a91885eb243ef09761a51233d28e
Christian Bulitta <github@bulitta.com>
2014-06-25 09:33:29 +0100

    switch to ambari 1.6.0 (working until reboot, if agent is deployed by server)

--- 04_implementation/provisioning/ambari-provisioning/modules/ambari_agent/manifests/init.pp
+++ 04_implementation/provisioning/ambari-provisioning/modules/ambari_agent/manifests/init.pp
@@ -6,37 +6,43 @@ class ambari_agent ($ownhostname, $serverhostname) {
 
   # Ambari Repo
   exec { 'get-ambari-agent-repo':
-    command => "wget http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.4.4.23/ambari.repo",
+    command => "wget http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.6.0/ambari.repo",
     cwd     => '/etc/yum.repos.d/',
     creates => '/etc/yum.repos.d/ambari.repo',
     user    => root
   }
 
-  # Ambari Agent
-  package { 'ambari-agent':
-    ensure  => present,
-    require => Exec[get-ambari-agent-repo]
+  # Root SSH-Keys von Vagrant
+  exec { 'copy-ssh-keys-to-root':
+    command => "mkdir -p /root/.ssh && cp /home/vagrant/.ssh/authorized_keys /root/.ssh/authorized_keys",
+    user    => root
   }
 
-  file_line { 'ambari-agent-ini-hostname':
-    ensure  => present,
-    path    => '/etc/ambari-agent/conf/ambari-agent.ini',
-    line    => "hostname=${serverhostname}", # server host name
-    match   => 'hostname=*',
-    require => Package[ambari-agent]
-  }
+  # Ambari Agent
+  #package { 'ambari-agent':
+  #  ensure  => present,
+  #  require => Exec[get-ambari-agent-repo]
+  #}
+
+  #file_line { 'ambari-agent-ini-hostname':
+  #  ensure  => present,
+  #  path    => '/etc/ambari-agent/conf/ambari-agent.ini',
+  #  line    => "hostname=${serverhostname}", # server host name
+  #  match   => 'hostname=*',
+  #  require => Package[ambari-agent]
+  #}
 
   exec { 'hostname':
     command => "hostname ${ownhostname}", # own host name
     user    => root
   }
 
-  exec { 'ambari-agent-start':
-    command => "ambari-agent start",
-    user    => root,
-    require => [Package[ambari-agent], Exec[hostname], File_line[ambari-agent-ini-hostname]],
-    onlyif  => 'ambari-agent status | grep "not running"'
-  }
+  #exec { 'ambari-agent-start':
+  #  command => "ambari-agent start",
+  #  user    => root,
+  #  require => [Package[ambari-agent], Exec[hostname], File_line[ambari-agent-ini-hostname]],
+  #  onlyif  => 'ambari-agent status | grep "not running"'
+  #}
   
   
 }
